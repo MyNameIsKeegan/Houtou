065c6f622c2cc406e9ab954dffe33cdd5175f158
Alienwear <keeganboldt@gmail.com>
2022-05-30 00:21:54 -0700

    E621

---
+++ Assets/Scripts/playerMove.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMove : MonoBehaviour
+{
+    private CircleCollider2D coll;
+    private Rigidbody2D rb;
+    public float sped;
+
+    void Awake()
+    {
+        coll = GetComponent<CircleCollider2D>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 dir;
+
+        dir.x = Input.GetAxisRaw("Horizontal");
+        dir.y = Input.GetAxisRaw("Vertical");
+
+        rb.MovePosition(rb.position + dir * Time.deltaTime * sped);
+    }
+}
