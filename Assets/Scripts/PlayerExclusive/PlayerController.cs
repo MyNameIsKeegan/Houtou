6ad4a50098ff89167a47d428e5609154e2ec374a
Skylarr <TheOtherSkylarr@gmail.com>
2022-05-31 00:25:09 -0700

    Add PlayerController + comment

---
+++ Assets/Scripts/PlayerExclusive/PlayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // SKY: This should act more as an control interface to other, more general components that we'll write. 
+    // I don't want too many custom things in here if we don't need them to be.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
