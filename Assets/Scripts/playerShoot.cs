3b506c111f8a598e1b2a9ffd4f7bb3c9e14e32e8
Alienwear <keeganboldt@gmail.com>
2022-07-18 15:58:52 -0700

    Auto stash before merge of "main" and "origin/main"

---
+++ Assets/Scripts/playerShoot.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerShoot : MonoBehaviour
+{
+    public Transform BulletOrifice;
+    public GameObject Bullet;
+    public float BulletSpeed = 10f;
+
+    void Update()
+    {
+        
+    }
+}
