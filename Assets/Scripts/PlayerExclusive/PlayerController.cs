7cf74d94cf652b2d37c1520a70c9c084baa42218
Skylarr Lacey <theotherskylarr@gmail.com>
2022-07-21 03:05:30 -0700

    Moved playermove into playercontroller

--- Assets/Scripts/PlayerExclusive/PlayerController.cs
+++ Assets/Scripts/PlayerExclusive/PlayerController.cs
@@ -4,18 +4,32 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // SKY: This should act more as an control interface to other, more general components that we'll write. 
-    // I don't want too many custom things in here if we don't need them to be.
+    // SKY: Instead of just an interface, we'll put anything that ONLY the player would need in here, including but not limited to controls.
 
+    // Privates
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+    private SpriteRenderer bulletSpongeBox;
+        // Shooting
     private Shooter shooter;
-
-    // Publics
+        // Movement
+    [Space]
+    [Header("Movement")]
+    private Vector2 inputDir;
+    public float basesped;
+    public float focusMultiplier = 0.15f;
+    public ModifierList spedMods = new ModifierList();
 
     void Awake()
     {
         shooter = GetComponent<Shooter>();
+        rb = GetComponent<Rigidbody2D>();
+
+        sprite = GetComponent<SpriteRenderer>();
+        bulletSpongeBox = transform.Find("PlayerBulletSponge").GetComponent<SpriteRenderer>();
     }
 
+    // Update is called once every frame
     void Update()
     {
         
@@ -23,5 +37,30 @@ public class PlayerController : MonoBehaviour
         {
             shooter.TryBoom();
         }
+
+        inputDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        
+        if (Input.GetKeyDown(KeyCode.LeftShift)) StartFocusing();
+        if (Input.GetKeyUp(KeyCode.LeftShift)) StopFocusing();
+    }
+
+    // FixedUpdate is called once every 0.02 seconds
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + inputDir * Time.deltaTime * (basesped * spedMods.Get()));
+    }
+
+    void StartFocusing()
+    {
+        sprite.color = new Color32(255, 177, 245, 127);
+        bulletSpongeBox.color = new Color32(255, 255, 255, 255);
+        spedMods.SetMod(focusMultiplier, "FocusMod");
+    }
+
+    void StopFocusing()
+    {
+        sprite.color = new Color32(255, 177, 245, 255);
+        bulletSpongeBox.color = new Color32(255, 255, 255, 0);
+        spedMods.RemoveMod("FocusMod");
     }
 }
